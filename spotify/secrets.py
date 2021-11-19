2e9b3f28a301e90ab1766c058c3db785c3ebf4cb
AbstractNucleus <noelhkleen@gmail.com>
2021-11-19 11:35:50 +0100

    save to csv

---
+++ spotify/secrets.py
@@ -0,0 +1,3 @@
+client_id = "166bf0427c28473385cfe53e316bec45"
+client_secret = "fb74405e87494854843b563eafa738d6"
+redirect_uri = "http://localhost:5000/callback"
---
+++ spotify/spotify_modules.py
@@ -0,0 +1,34 @@
+import spotipy, json, csv
+from secrets import client_secret, client_id, redirect_uri
+import numpy as np
+import pandas as pd
+
+sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+
+def save_csv(user):
+    playlists = sp.user_playlists(user)["items"]
+    print(f"{user}'s playlists:")
+    playlist_ids = []
+    x = 1
+
+    for i in playlists:
+        print(f"   {x}. "+i["name"]); x += 1
+        playlist_ids.append(i["id"])
+    wanted_pl = input("Enter number of wanted playlist: ")
+
+    pl_id = playlist_ids[int(wanted_pl)-1]
+    get_tracks = sp.playlist_items(pl_id)["items"]
+    tracks = []
+    track_ids = []
+    for i in get_tracks:
+        tracks.append(i["track"]["name"])
+        track_ids.append(i["track"]["id"])
+
+    features = sp.audio_features(track_ids)
+    df = pd.DataFrame(features)
+
+    df.to_csv(f"spotify/data/{pl_id}.csv")
+    with open(f"spotify/data/{pl_id}.json", "w") as j: j.write(json.dumps(features, indent=4))
+        
+
+save_csv("weepur007")
