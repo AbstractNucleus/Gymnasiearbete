da9fde0cc8084972d42a2dfc49aa34126dcc6b58
AbstractNucleus <noelhkleen@gmail.com>
2021-11-25 15:01:12 +0100

    Please finish S

--- DES/des_extended.py
+++ DES/des_extended.py
@@ -1,17 +1,39 @@
 from tables import *
 
-def permutate(string, table, output = ""):
+
+# En funnktion för att permutera en string med ett table
+def permutate(string, table):
+    output = ""
     for i in range(len(table)):
         output += str(string[table[i]-1])
     return output
 
-def translate(string, table, output = ""):
+
+# En funktion för att översätta med ett table som är en dictionary
+def translate(string, table):
+    output = ""
     for i in string:
         output += str(table[i])
     return output
 
 
-M = list(str(1030114186110932))
+# Används för att generera L och R
+def f(r, k):
+    r = permutate(r, E_BIT)
+    return xor(r, k, len(r))
+
+
+# XOR funktion som går att använda med strings
+def xor(a, b, n):
+    ans = ""
+    for i in range(n):
+        if (a[i] == b[i]):
+            ans += "0"
+        else:
+            ans += "1"
+    return ans
+
+M = "1030114186110932"
 
 
 
@@ -45,64 +67,24 @@ for i in range(1, 17):
 
 # Permuterar binära M med IP och skapar L_0 och R_0
 M_IP = permutate(M_BIN, IP)
-L, R = [M_IP[:32]], [M_IP[32:]]
+L, R = M_IP[:32], M_IP[32:]
+L_LIST, R_LIST = [L], [R]
 
 
 # Next in the f calculation, we XOR the output E(Rn-1) with the key Kn
-# Let + denote XOR addition, (bit-by-bit addition modulo 2). Then for n going from 1 to 16 we calculate
+# Ln = Rn-1
+# Rn = Ln-1 + f(Rn-1,Kn)
 
-#Ln = Rn-1
-#Rn = Ln-1 + f(Rn-1,Kn)
+for n in range(1, 17):
+    L_LIST.append(R_LIST[n-1])
+    R_LIST.append(L_LIST[n-1] + f(R_LIST[n-1], K_LIST[n-1]))
+    
 
-# 1. Permutera Rn-1 med E
+RL_LIST = []
+for i in range(len(R_LIST)):
+    RL_LIST.append(R_LIST[i] + L_LIST[i])
 
+for i in R_LIST:
+    print(i)
 
-
-
-E_0_K_1 = ""
-for i in range(48):
-    if int(E[0][i]) == 1:
-        if int(pc_table[1][i]) == 0:
-            E_0_K_1 += "1"
-        elif int(pc_table[1][i]) == 1:
-            E_0_K_1 += "0"
-    if int(E[0][i]) == 0:
-        if int(pc_table[1][i]) == 0:
-            E_0_K_1 += "0"
-        elif int(pc_table[1][i]) == 1:
-            E_0_K_1 += "1"
-
-E_0_list = []
-for i in range(0, 48, 6):
-    E_0_list.append(E_0_K_1[i:i+6])
-
-
-# Nästa steg är att göra E_0
-gg = {
-    "00": "0",
-    "01": "1",
-    "10": "2",
-    "11": "3",
-    "0000": "0",
-    "0001": "1",
-    "0010": "2",
-    "0011": "3",
-    "0100": "4",
-    "0101": "5",
-    "0110": "6",
-    "0111": "7",
-    "1000": "8",
-    "1001": "9",
-    "1010": "10",
-    "1011": "11",
-    "1100": "12",
-    "1101": "13",
-    "1110": "14",
-    "1111": "15"
-}
-r = []
-for i in E_0_list:
-    r.append(gg[i[0]+i[5]])
-    r.append(gg[i[1]+i[2]+i[3]+i[4]])
-print(r)
-
+#print(hex(int(permutate(RL_LIST[16], IP_1), 2)))
