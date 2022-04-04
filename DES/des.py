9a0fb9c14941531f16a1bdd1471fcf08d6ce3c45
AbstractNucleus <noelhkleen@gmail.com>
2022-04-04 22:40:24 +0200

    input

--- DES/des.py
+++ DES/des.py
@@ -1,9 +1,20 @@
 if __name__ == "__main__":
+    y = " "
     from des_modules import *
-
-    MSG = "1030114186110932"
-    KEY = "0001001100110100010101110111100110010011001000010000010001010001"
-    BIN_KEYS = generate_keys(KEY)
-    CIPHER = encrypt(MSG, BIN_KEYS)
-    TEXT = decrypt(CIPHER, BIN_KEYS)
-    print(TEXT)
+    MSG = input("hej")
+    print(MSG[0])
+    for i in range(len(MSG)):
+        y += str(ord(MSG[i]))
+    #print(y)
+    while len(y) < 64:
+        y += str(0)
+    if len(y) > 64:
+        y = y[0:64]
+    MSG = y
+    print(y)
+    #MSG = "1030114186110932"
+    #KEY = "0001001100110100010101110111100110010011001000010000010001010001"
+    #BIN_KEYS = generate_keys(KEY)
+    #CIPHER = encrypt(MSG, BIN_KEYS)
+    #TEXT = decrypt(CIPHER, BIN_KEYS)
+    #print(TEXT)
--- RSA/rsa.py
+++ RSA/rsa.py
@@ -1,4 +1,6 @@
 if __name__ == "__main__":
     from rsa_modules import *
-    message = 1030114186110932
+    #message = 1030114186110932
+    message = int(input("Skriv in ditt meddelande. Det ska vara 16 bokstäver långt."))
     p, q, F, n, e, d = key_gen(128)
+    svar = encrypt(message, n, e)
